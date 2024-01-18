54ae251de3b4e48994fab27a12896efc721739b9
JasonFurmage <150832588+JasonFurmage@users.noreply.github.com>
2024-01-18 19:31:23 +0000

    Added form validation functions and error messages.

--- js/app.js
+++ js/app.js
@@ -1,19 +1,27 @@
+/* Animate Banner Headings
+   ========================================================================== */
+
 let i = 0;
-let heading = $('#heading').text(); // Get current page heading.
+let heading = $('#js-heading').text(); // Get current page heading.
 
 // Make heading empty and sub heading invisible to set up effects.
-$('#heading').text("");
-$('#sub-heading').css("opacity",  0);
+$('#js-heading').text("");
+$('#js-subheading').css("opacity",  0);
 
 function showBannerHeadings() {
   if (i < heading.length) {
-    $("#heading").append(heading.charAt(i));
+    $("#js-heading").append(heading.charAt(i));
     i++;
     setTimeout(showBannerHeadings, 100);
   } else {
-    $('#sub-heading').animate({opacity: 1}, 1000);
+    $('#js-subheading').animate({opacity: 1}, 1000);
   }
 }
+
+showBannerHeadings();
+
+/* Toggle Sidebar
+   ========================================================================== */
 
 let sidebarVisible = false;
 
@@ -22,8 +30,82 @@ function toggleSideBar() {
   sidebarVisible = !sidebarVisible; // Check if sidebar is currently on screen.
 
   // Toggle visibility and width.
-  document.getElementById("js-sidebar").style.visibility = sidebarVisible ? "visible" : "hidden" ;
-  document.getElementById("js-sidebar").style.width = sidebarVisible ? "240px" : "0" ;
+  $("#js-sidebar").css("visibility", sidebarVisible ? "visible" : "hidden");
+  $("#js-sidebar").css("width", sidebarVisible ? "240px" : "0");
 }
 
-showBannerHeadings();
+/* Form Validation
+   ========================================================================== */
+
+let errorMessage;
+
+function validateForm() {
+
+  let isFormValid = false;
+  let formErrorEl = $('#js-error'); // Div for displaying error message.
+  formErrorEl.hide(); // Hide initially to give feedback whenever submit button is clicked.
+
+  // Get input fields.
+  const firstNameEl = $("#js-form input[name='first_name']");
+  const lastNameEl = $("#js-form input[name='last_name']");
+  const emailEl = $("#js-form input[name='email']");
+  const subjectEl = $("#js-form input[name='subject']");
+  const messageEl = $("#js-form textarea[name='message']");
+
+  // Check if values are valid.
+  isEmailValid = isValid(emailEl);
+  isFirstNameValid = isValid(firstNameEl);
+  isLastNameValid = isValid(lastNameEl);
+  isSubjectValid = isValid(subjectEl);
+  isMessageValid = isValid(messageEl);
+
+  // Check if all values are valid.
+  isFormValid = isFirstNameValid && isLastNameValid && isEmailValid && isSubjectValid && isMessageValid;
+
+  // Display error or confirmation message.
+  if (!isFormValid) {
+
+    formErrorEl.text(errorMessage);
+    formErrorEl.fadeIn(1000);
+
+  } else {
+    alert("Your message has been sent.")
+  }
+
+  return isFormValid;
+}
+
+function isValid(input) {
+  
+  let error = false;
+
+  // Check for valid email address.
+  if ($(input).is('input[type="email"]')) {
+    if (!isValidEmail(input.val())) {
+      error = true;
+      errorMessage = "Please enter a valid email address."
+    }
+  } 
+    
+  // Check for empty text field.
+  if (isEmpty(input.val())) {
+    error = true;
+    errorMessage = "Please fill in all required fields."
+  }
+
+  // Add error class to input fields if errors are found.
+  error ? input.addClass("error") : input.removeClass("error");
+
+  return !error;
+}
+
+// Check email with regex.
+function isValidEmail(value) {
+  const regex = /^[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+  return regex.test(value);
+}
+
+// Check if string is empty.
+function isEmpty(value) {
+  return value === '';
+}
