576cf2da0f7d52fb19484a6bf29e3d0898054868
JasonFurmage <150832588+JasonFurmage@users.noreply.github.com>
2024-01-22 13:11:16 +0000

    Updated email regex to accept subdomains and multiple levels.

--- js/app.js
+++ js/app.js
@@ -118,7 +118,7 @@ function isValid(input) {
 
 // Check email with regex.
 function isValidEmail(value) {
-  const regex = /^[a-zA-Z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+\/=?^_`{|}~-]+)*@[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?$/
+  const regex = /^[a-zA-Z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}$/;
   return regex.test(value);
 }
 
